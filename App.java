0b47e665e358d6f2b716a14c9010bfd0109f7b7f
JohnAndrieu <jonathan.andrieu11@gmail.com>
2020-12-09 09:52:14 +0100

    un petit changement

--- App.java
+++ App.java
@@ -10,5 +10,6 @@ public class App {
         int a = 3;
         int b = 5;
         System.out.println("La somme de "+a+" et "+b+" est : "+app.somme(a, b));
+        System.out.println("Un petit changement");
     }
 }
